24631abdb99260f2d476f40319624fa6d571f95f
GreyAudron <mikhail.schee@mail.utoronto.ca>
2026-06-30 08:53:06 -0400

    data: add `sivol` variable for HadGEM3 and EC-Earth3P-HR models

--- esgpull_searches/EC-Earth3P-HR_sivars_hist-1950.sh
+++ esgpull_searches/EC-Earth3P-HR_sivars_hist-1950.sh
@@ -5,6 +5,6 @@ data_node:eagle.alcf.anl.gov,esgf-node.ornl.gov \
 experiment_id:hist-1950 \
 source_id:EC-Earth3P-HR \
 frequency:mon \
-variable_id:siage,siconc,sispeed,sithick,siu,siv
-# Expect to find 36 datasets.
-# Downloads 975 files (28.7 GiB)
+variable_id:siage,siconc,sispeed,sithick,siu,siv,sivol
+# Expect to find 42 datasets.
+# Downloads 1365 files (33.4 GiB)
