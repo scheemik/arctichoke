35bee70abcac41bb76576b2277a3913b9ac4729f
GreyAudron <mikhail.schee@mail.utoronto.ca>
2026-07-15 17:09:10 -0400

    feat(trend_in_time): use `scipy.stats.linregress` for `trend_in_time` to get p-value outputs

--- src/arctichoke/analysis/trend_in_time.py
+++ src/arctichoke/analysis/trend_in_time.py
@@ -1,10 +1,303 @@
 import numpy as np
+from scipy.stats import linregress
 import xarray as xr
 
 from arctichoke import get_current_datetime_str
 from arctichoke.dataset import get_variable_name, get_min_max, get_epoch_times, get_date_type
 import arctichoke.params as sps
 from arctichoke.verify import verify_path
+
+def trend_in_time(
+    dataset: (str, [str], xr.Dataset, xr.DataArray),
+    var: str = None,
+    time_dim: str = 'year',
+    mask_where_zero_across_time: bool = False,
+    use_xarray_polyfit: bool = True,
+    save_as: str = None,
+    verbose: bool = False,
+    **kwargs,
+):
+    """ Find the trend for each grid cell along the time axis.
+
+        For each grid cell in the dataset, find the trend in time for the given variable. 
+        This results in a new dataset without a `time` dimension. 
+
+        Parameters
+        ----------
+        dataset : `str`, list of `str`, `xarray.Dataset`, `xarray.DataArray`
+            The dataset of which to find the sum across time.
+        var : `str`, `None`, optional
+            The variable in `dataset` for which to take the trend across time.
+            This is required if `dataset` is an `xarray.Dataset`. 
+            Default is `None`.
+        time_dim : `str`, optional
+            The name of the time dimension over which to find the trend.
+            Default is `year`. 
+        mask_where_zero_across_time : `bool`, optional
+            Whether to mask out grid cells which have zero as a value across the entire time dimension using `mask_where_all_zero()`.
+            Default is `False`. 
+        use_xarray_polyfit : `bool`, optional
+            If `True`, use `xarray.polyfit()` to take trends in time, which skips `nan` values. 
+            If `False`, use `numpy.polyfit()` to take trends in time, which has `nan` values invalidate an index of the array across all time.
+            Default is `True`.
+        save_as : `str`, `None`, optional
+            The file name to which to save the modified dataset.
+            Default is `None`, which doesn't save the dataset to a file.
+        verbose : `bool`, optional
+            Whether to verbosely output information as the function executes.
+            Default is `False`.
+        **kwargs
+            Keyword arguments to pass to `xr.sum()`.
+
+        Returns
+        -------
+        dataset : `xarray.Dataset`
+            A dataset with the trends in time for the specified variable.
+        
+        Examples
+        --------
+        >>> from arctichoke.dataset.example_dataset import make_example_dataset
+        >>> from arctichoke.path.manipulate_paths import make_file_path
+        >>> # Create multiple example test files
+        >>> test_file_dir = 'tests/test_analysis/example_datasets'
+        >>> make_file_path(test_file_dir)
+        >>> test_file_names = [
+        >>>     f"{test_file_dir}/example_dataset_0.nc",
+        >>>     f"{test_file_dir}/example_dataset_1.nc",
+        >>>     f"{test_file_dir}/example_dataset_2.nc",
+        >>> ]
+        >>> offsets = [0, 1, 3]
+        >>> for i in range(len(test_file_names)):
+        >>>     make_example_dataset(
+        >>>         n=3,
+        >>>         offset=offsets[i],
+        >>>         test_var_name='test_var',
+        >>>         time_axis=(2000+i),
+        >>>         save_as=test_file_names[i],
+        >>>     )
+        >>> import xarray as xr
+        >>> test_dataset = xr.open_mfdataset(test_file_names)
+        >>> test_dataset['test_var'].values
+        array([[[ 0.,  1.,  2.],
+                [ 3.,  4.,  5.],
+                [ 6.,  7.,  8.]],
+               [[ 0.,  1.,  2.],
+                [ 3.,  4.,  5.],
+                [ 6.,  7.,  8.]],
+               [[ 1.,  2.,  3.],
+                [ 4.,  5.,  6.],
+                [ 7.,  8.,  9.]],
+               [[ 1.,  2.,  3.],
+                [ 4.,  5.,  6.],
+                [ 7.,  8.,  9.]],
+               [[ 3.,  4.,  5.],
+                [ 6.,  7.,  8.],
+                [ 9., 10., 11.]],
+               [[ 3.,  4.,  5.],
+                [ 6.,  7.,  8.],
+                [ 9., 10., 11.]]])
+        >>> from arctichoke.analysis.trend_in_time import trend_in_time
+        >>> test_trends = trend_in_time(
+        >>>     test_dataset,
+        >>>     var='test_var',
+        >>>     time_dim='time',
+        >>> )
+        >>> test_trends['test_var_trends'].values
+        array([[1.49369, 1.49369, 1.49369],
+               [1.49369, 1.49369, 1.49369],
+               [1.49369, 1.49369, 1.49369]])
+    """
+    # Verify input arguments
+    if not isinstance(verbose, bool):
+        raise TypeError(f"(trend_in_time) `verbose` must be a `bool`. Got type: {type(verbose)}")
+    if isinstance(dataset, str):
+        # Wrap that string into a list
+        dataset = [dataset]
+    if isinstance(dataset, type([])):
+        if len(dataset) < 1:
+            raise ValueError(f"(trend_in_time) `dataset` must have at least one item. Got: {dataset}")
+        for datafile in dataset:
+            if not isinstance(datafile, str):
+                raise TypeError(f"(trend_in_time) Each item in `dataset` list must be a string. Got: {type(datafile)}")
+            # Verify this is a valid path
+            datafile = verify_path(datafile)
+            if not datafile.endswith('.nc'):
+                raise TypeError(f"(plot_time_series) `datafile` must be a `.nc` filepath. Got: {datafile}")
+        # Load all the files at once
+        if verbose:
+            print(f"(trend_in_time) When passing a list of files, ensure their coordinates match as that is not verified in this function.")
+        dataset = xr.open_mfdataset(dataset)
+    elif not isinstance(dataset, (xr.Dataset, xr.DataArray)):
+        raise TypeError(f"(trend_in_time) `dataset` must be a string, `xr.Dataset`, or `xarray.DataArray`. Got type: {type(dataset)}")
+    if not isinstance(var, (str, type(None))):
+        raise TypeError(f"(trend_in_time) `var` must be a string or `None`. Got type: {type(var)}")
+    if not isinstance(time_dim, str):
+        raise TypeError(f"(trend_in_time) `time_dim` must be a string. Got type: {type(time_dim)}")
+    if not isinstance(mask_where_zero_across_time, bool):
+        raise TypeError(f"(trend_in_time) `mask_where_zero_across_time` must be a `bool`. Got type: {type(mask_where_zero_across_time)}")
+    if not isinstance(use_xarray_polyfit, bool):
+        raise TypeError(f"(trend_in_time) `use_xarray_polyfit` must be a `bool`. Got type: {type(use_xarray_polyfit)}")
+    if not isinstance(save_as, (str, type(None))):
+        raise TypeError(f"(trend_in_time) `save_as` must be a string or `None`. Got type: {type(save_as)}")
+    elif isinstance(save_as, str) and not '.nc' in save_as:
+        raise TypeError(f"(trend_in_time) `save_as` must be a `.nc` filepath. Got: {save_as}")
+
+    # Verify `dataset` has the specified variable
+    if isinstance(dataset, xr.Dataset):
+        actual_vars = get_variable_name(dataset)
+        if var not in actual_vars:
+            raise ValueError(f"(trend_in_time) `dataset` must have the specified `var` {var}. Available variables: {actual_vars}")
+    else:
+        # Get the name of the variable
+        var = dataset.name
+        # Convert `dataset` from `xr.DataArray` to `xr.Dataset`
+        dataset = dataset.to_dataset()
+
+    if time_dim != 'year':
+        raise NotImplementedError(f"(trend_in_time) Trend calculation not yet implemented for `time_dim`: {time_dim}")
+    
+    # Mask grid cells which have values of zero over all time
+    if mask_where_zero_across_time:
+        dataset = mask_where_all_zero(
+            dataset,
+            var,
+            time_dim,
+            verbose,
+        )
+    
+    # Rechunk to allow efficient calculation
+    if verbose:
+        print(f"(trend_in_time) Rechunking along '{time_dim}' dimension.")
+    dataset = dataset.chunk({time_dim: -1, "i": "auto", "j": "auto"})
+    
+    # Information to output
+    if verbose:
+        print(f"(trend_in_time) `save_as`: {save_as}")
+
+    # Store the variable attributes to put back later
+    var_attrs = dataset[var].attrs
+
+    # Get the trends and statistics
+    trends_dataset = xr.apply_ufunc(
+        new_linregress, dataset[time_dim], dataset[var],
+        input_core_dims=[[time_dim], [time_dim]],
+        output_core_dims=[["parameter"]],
+        vectorize=True,
+        dask="parallelized",
+        output_dtypes=['float64'],
+        output_sizes={"parameter": 5},
+    )
+    trends_dataset = trends_dataset.to_dataset()
+    # trends_dataset = trends_dataset.drop_vars('year')
+
+    linregress_attrs_dict = {
+        'slope': {
+            'standard_name': f'{var}_trends',
+            'long_name': 'Trend in ',
+            'units': '/yr',
+            'comment': 'Trend in ',
+            'history': f"{get_current_datetime_str()} altered by `arctichoke`: Calculated trends across `{time_dim}` of `{var}` values to get `{var}_trends`.",
+        },
+        'pvalue': {
+            'standard_name': f'{var}_pvalue',
+            'long_name': 'P-value of trend in ',
+            'units': '/yr',
+            'comment': 'P-value of trend in ',
+            'history': f"{get_current_datetime_str()} altered by `arctichoke`: Calculated trends across `{time_dim}` of `{var}` values to get `{var}_trends`.",
+        },
+    }
+    
+    # Set `dataset` to be just the first time slice
+    dataset = dataset.isel({time_dim:0}, drop=True)
+
+    # Rename the variable, giving it the suffix `_trends`
+    dataset = dataset.rename_vars({var: f'{var}_trends'})
+    # Put the trends into the original dataset
+    dataset[f'{var}_trends'] = trends_dataset['year'].isel(parameter=0)
+    dataset[f'{var}_pvalue'] = trends_dataset['year'].isel(parameter=3)
+    # Restore the variable attributes
+    dataset[f'{var}_trends'].attrs = var_attrs
+    # Add this operation to the history
+    if 'history' in dataset.attrs.keys():
+        original_history = dataset.attrs['history']
+    else:
+        original_history = ''
+    dataset.attrs['history'] = f"{get_current_datetime_str()} altered by `arctichoke`: Calculated trends across `{time_dim}` of `{var}` values to get `{var}_trends`. {original_history}"
+    # Get references to these new variables
+    xr_var_trends = dataset[f'{var}_trends']
+    xr_var_resids = dataset[f'{var}_pvalue']
+        
+    if verbose:
+        print(f"(trend_in_time) Modifing dataset attributes")
+    # Modify the attributes of the dataset to reflect the changes
+    xr_var_trends.attrs['standard_name'] = f'{var}_trends'
+    xr_var_resids.attrs['standard_name'] = f'{var}_pvalue'
+    if 'long_name' in xr_var_trends.attrs.keys():
+        xr_var_trends.attrs['long_name'] = f'Trend in {xr_var_trends.attrs['long_name']}'
+    else:
+        xr_var_trends.attrs['long_name'] = f'Trend in {var}'
+    xr_var_resids.attrs['long_name'] = f'P-value of trend in {var}'
+    if 'units' in xr_var_trends.attrs.keys():
+        xr_var_trends.attrs['units'] = f'{xr_var_trends.attrs['units']}/yr'
+    else:
+        xr_var_trends.attrs['units'] = f'N/P'
+    xr_var_resids.attrs['units'] = f'({xr_var_trends.attrs['units']})^2'
+    if 'comment' in xr_var_trends.attrs.keys():
+        xr_var_trends.attrs['comment'] = f'Trend in {xr_var_trends.attrs['comment']}'
+    else:
+        xr_var_trends.attrs['comment'] = f'N/P'
+    xr_var_resids.attrs['comment'] = f'P-value for the trend in {var}'
+    xr_var_trends.attrs['original_name'] = f'{var}_trends'
+    xr_var_resids.attrs['original_name'] = f'{var}_pvalue'
+    if 'history' in xr_var_trends.attrs.keys():
+        original_history = xr_var_trends.attrs['history']
+    else:
+        original_history = ''
+    xr_var_trends.attrs['history'] = f"{get_current_datetime_str()} altered by `arctichoke`: Calculated trends across `{time_dim}` of `{var}` values to get `{var}_trends`. {original_history}"
+
+    # Save the modified dataset, if applicable
+    if not isinstance(save_as, type(None)):
+        if verbose:
+            print(f"(trend_in_time) Saving the dataset file: {save_as}")
+        # Save the plot to file
+        dataset.to_netcdf(save_as)
+    
+    return dataset
+
+# See: https://stackoverflow.com/questions/52094320/with-xarray-how-to-parallelize-1d-operations-on-a-multidimensional-dataset
+def new_linregress(
+    x, 
+    y,
+):
+    """ Wrapper around `scipy.stats.linregress` to use in `apply_ufunc`.
+
+        A user function to use in `xarray.apply_ufunc` to facilitate running `scipy.stats.linregress` in a vectorized manner over `xarray.Dataset`'s.
+
+        Parameters
+        ----------
+        x : `xarray.DataArray`
+            The data array to use in as the indepenent data in `linregress`.
+        y : `xarray.DataArray`
+            The data array to use in as the depenent data in `linregress`.
+
+        Returns
+        -------
+        output_arr : Array of values
+            An array containing the outputs of `linregress`, namely `['slope', 'intercept', 'rvalue', 'pvalue', 'std_err']`.
+        
+        Examples
+        --------
+    """
+    # Verify input arguments
+    if not isinstance(x, np.ndarray):
+        raise TypeError(f"(new_linregress) `x` must be `xr.DataArray`. Got type: {type(x)}")
+    if not isinstance(y, np.ndarray):
+        raise TypeError(f"(new_linregress) `y` must be `xr.DataArray`. Got type: {type(y)}")
+
+    # Wrapper around scipy linregress to use in apply_ufunc
+    slope, intercept, r_value, p_value, std_err = linregress(x, y)
+    output_arr = np.array([slope, intercept, r_value, p_value, std_err])
+    return output_arr
 
 def trend_in_time_old(
     dataset: (str, [str], xr.Dataset, xr.DataArray),
