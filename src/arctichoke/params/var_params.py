733c9520e02232351b9e850ecf4227a807ab7bfa
GreyAudron <mikhail.schee@mail.utoronto.ca>
2026-07-27 14:22:46 -0400

    feat(var_params): add `marker_var` attribute to `sea_ice_vars` dictionary to indicate whether a variable marks a simple binary or not

--- src/arctichoke/params/var_params.py
+++ src/arctichoke/params/var_params.py
@@ -3,33 +3,60 @@
 sea_ice_vars = {
     'test_var': {
         'plot_range': None,
+        'marker_var': False,
     },
     'siage': {
         'plot_range': [0, 5],
+        'marker_var': False,
     },
     'siage2': {
         'plot_range': [0, 10],
+        'marker_var': False,
     },
     'siconc': {
         'plot_range': [0, 100],
+        'marker_var': False,
     },
     'siconc2': {
         'plot_range': [0, 100],
+        'marker_var': False,
     },
     'sispeed': {
         'plot_range': None,
+        'marker_var': False,
     },
     'sithick': {
         'plot_range': [0, 10],
+        'marker_var': False,
     },
     'siu': {
         'plot_range': None,
+        'marker_var': False,
     },
     'siv': {
         'plot_range': None,
+        'marker_var': False,
     },
     'sivol': {
         'plot_range': [0, 10],
+        'marker_var': False,
+    },
+    # Marker variables
+    'silandfast': {
+        'plot_range': None,
+        'marker_var': True,
+    },
+    'simultiyear': {
+        'plot_range': None,
+        'marker_var': True,
+    },
+    'sipacked': {
+        'plot_range': None,
+        'marker_var': True,
+    },
+    'sislow': {
+        'plot_range': None,
+        'marker_var': True,
     },
 }
 
