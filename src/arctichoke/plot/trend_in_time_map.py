acee26da4f09a1357995fea8c788d13589bd565d
GreyAudron <mikhail.schee@mail.utoronto.ca>
2026-08-14 15:52:23 -0400

    style(make_trend_map): remove unnecessary import of `make_title()`

--- src/arctichoke/plot/trend_in_time_map.py
+++ src/arctichoke/plot/trend_in_time_map.py
@@ -4,7 +4,7 @@ from arctichoke.analysis import sum_by_year, trend_in_time, trend_in_time_scipy
 from arctichoke.dataset import select_months
 import arctichoke.params as sps
 from arctichoke.path import list_variable_files
-from arctichoke.plot import make_title, quadmesh_map
+from arctichoke.plot import quadmesh_map
 
 def make_trend_map(
     this_source_id: str,
