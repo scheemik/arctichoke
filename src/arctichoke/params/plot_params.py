1f7444d323db556350020a138d6605c22ae77b84
GreyAudron <mikhail.schee@mail.utoronto.ca>
2026-08-17 15:33:24 -0400

    feat(plot_params): add a list of colorblind-friendly color codes

---
+++ src/arctichoke/params/plot_params.py
@@ -0,0 +1,15 @@
+# Define parameters for the plots made in this project
+
+# Seaborn `colorblind` palette
+sb_clrs = [
+    '#3171AD',      # Dark blue
+    '#D29233',      # Light orange
+    '#469B76',      # Green
+    '#C66526',      # Dark orange
+    '#C07CB8',      # Dark pink
+    '#C19468',      # Light brown
+    '#F0B2E1',      # Light pink
+    '#949494',      # Gray
+    '#EAE159',      # Yellow
+    '#6EB2E4',      # Light blue
+]
