b0eb25642cbf6f7cdc019454bfa5763f453b8697
GreyAudron <mikhail.schee@mail.utoronto.ca>
2026-08-17 17:19:01 -0400

    feat(plot_multi_time_series): add function to plot a time series with all available variant labels for the given model

--- src/arctichoke/plot/__init__.py
+++ src/arctichoke/plot/__init__.py
@@ -7,4 +7,5 @@ from .limit_extent import *
 from .save_hvplots import *
 from .seasonal_cycle import *
 from .time_series import *
+from .time_series_multi import *
 from .trend_in_time_map import *
